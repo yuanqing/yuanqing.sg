6b5d6f2cc5669e26ee967de9d2d07b5dc0bf739a
yuanqing <desk@yuanqing.sg>
2021-03-16 00:08:48 +0800

    Add script to check for unused CSS

---
+++ scripts/check-unused-css.ts
@@ -0,0 +1,22 @@
+import * as purifyCss from 'purify-css'
+
+function main(): void {
+  const args = process.argv.slice(2)
+  if (args.length < 2) {
+    throw new Error('Need glob for HTML and a glob for CSS')
+  }
+  const html = [args[0]]
+  const css = [args[1]]
+  const output = []
+  // eslint-disable-next-line no-console
+  console.log = function (...args: Array<any>) {
+    output.push(args.join(' '))
+  }
+  purifyCss(html, css, {
+    rejected: true
+  })
+  if (output.length > 0) {
+    throw new Error(`Unused CSS\n${output.join('')}`)
+  }
+}
+main()
