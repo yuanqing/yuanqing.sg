48ce59cdc4fced27ca9d91f76d1ffdb285579350
yuanqing <desk@yuanqing.sg>
2021-04-10 18:20:18 +0800

    Fix Medium scraping

--- scripts/fetch-stats/utilities/fetch-medium-stats-async.ts
+++ scripts/fetch-stats/utilities/fetch-medium-stats-async.ts
@@ -1,27 +1,36 @@
-import { JSDOM } from 'jsdom'
-
-const fetch = require('isomorphic-unfetch')
+import * as pProps from 'p-props'
+import * as puppeteer from 'puppeteer'
 
 export async function fetchMediumStatsAsync(urls: {
   [key: string]: string
 }): Promise<{ [key: string]: string }> {
-  const result: { [key: string]: string } = {}
+  const browser = await puppeteer.launch()
+  const promises = {}
   for (const id in urls) {
-    result[id] = await fetchMediumArticleClapCountAsync(urls[id])
+    promises[id] = fetchMediumArticleClapCountAsync(id, urls[id], browser)
   }
+  const result: { [key: string]: string } = await pProps(promises)
+  await browser.close()
   return result
 }
 
-async function fetchMediumArticleClapCountAsync(url: string): Promise<string> {
-  const response = await fetch(url)
-  const html = await response.text()
-  const document = new JSDOM(html).window.document
-  const clapSvgElement = document.querySelector(
-    '[data-test-id="post-sidebar"] [aria-label="clap"]'
-  )
-  let parentElement = clapSvgElement
-  while (parentElement.children.length === 1) {
-    parentElement = parentElement.parentElement
-  }
-  return parentElement.textContent.trim()
+async function fetchMediumArticleClapCountAsync(
+  id: string,
+  url: string,
+  browser: puppeteer.Browser
+): Promise<string> {
+  console.log(`Medium: ${id}`) // eslint-disable-line no-console
+  const page = await browser.newPage()
+  await page.goto(url)
+  const selector = '[data-test-id="post-sidebar"] [aria-label="clap"]'
+  await page.waitForSelector(selector, { timeout: 10000 })
+  const count = await page.$$eval(selector, function (elements) {
+    let parentElement = elements[0]
+    while (parentElement.children.length === 1) {
+      parentElement = parentElement.parentElement
+    }
+    return parentElement.textContent.trim()
+  })
+  await page.close()
+  return count
 }
